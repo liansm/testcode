8a6756a8a1e6a390dc705e15036314832acc8ddf
liansm <liansm@gmail.com>
2023-04-23 07:35:04 +0800

    many six changes

--- exam/many_six.cpp
+++ exam/many_six.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
 #include <vector>
 
-bool is_match(std::string& sub_str)
-{
-	int val = atoi(sub_str.c_str());
-	return val != 0 && val % 6 == 0;
-}
-
 int main()
 {
 	std::string n;
 	std::cin >> n;
 	int count = 0;
 	for (size_t i = 0; i < n.size(); ++i) {
-		std::string sub_str;
-		sub_str.push_back((n[i]));
-		if (is_match(sub_str)) {
-			count++;
+		int sum = 0;
+		int cur_val = n[i] - '0';
+		if (cur_val % 6 == 0) {
+			count ++;
 		}
-		
-		for (int j = i+1; j < n.size(); ++j) {
-			sub_str.push_back(n[j]);
-			if (is_match(sub_str)) {
+		sum += cur_val;
+		for (int j = i + 1; j < n.size(); ++j) {
+			cur_val = n[j] - '0';
+			sum += cur_val;
+			if (cur_val % 2 == 0 && sum % 3 == 0) {
 				count++;
 			}
+
 		}
 	}
 	std::cout << count;
 	return 0;
 }
-
-
